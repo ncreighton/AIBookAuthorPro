7fdb6f1e4e56eacf59978b3e8d419cf065a82b72
ncreighton <n.creighton14@gmail.com>
2025-11-28 00:07:38 -0500

    Fix: Export dialog close button not working
    - Subscribe to CloseRequested event from ViewModel
    - Use DialogHost.CloseDialogCommand to close properly
    - Add proper cleanup on unload

--- src/AIBookAuthorPro.UI/Views/ExportDialogView.xaml.cs
+++ src/AIBookAuthorPro.UI/Views/ExportDialogView.xaml.cs
@@ -3,8 +3,11 @@
 // Copyright (c) 2024 Nick Creighton. All rights reserved.
 // =============================================================================
 
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using AIBookAuthorPro.UI.ViewModels;
+using MaterialDesignThemes.Wpf;
 using UserControl = System.Windows.Controls.UserControl;
 
 namespace AIBookAuthorPro.UI.Views;
@@ -25,5 +28,62 @@ public partial class ExportDialogView : UserControl
         DataContext = _viewModel;
         
         InitializeComponent();
+
+        // Subscribe to the close request from ViewModel
+        _viewModel.CloseRequested += OnCloseRequested;
+
+        // Unsubscribe when unloaded
+        Unloaded += OnUnloaded;
+    }
+
+    private void OnUnloaded(object sender, RoutedEventArgs e)
+    {
+        // Unsubscribe to prevent memory leaks
+        _viewModel.CloseRequested -= OnCloseRequested;
+    }
+
+    private void OnCloseRequested(object? sender, EventArgs e)
+    {
+        // Find the DialogHost that contains this UserControl and close it
+        var dialogHost = FindParentDialogHost(this);
+        
+        if (dialogHost != null)
+        {
+            // Close the dialog using the instance method
+            dialogHost.IsOpen = false;
+        }
+        else
+        {
+            // Fallback: Try to close using the DialogHost command
+            // This works when the dialog was opened with DialogHost.Show()
+            try
+            {
+                DialogHost.CloseDialogCommand.Execute(false, this);
+            }
+            catch
+            {
+                // Last resort: Try the static close method
+                DialogHost.Close("MainDialogHost", false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Traverses the visual tree to find the parent DialogHost.
+    /// </summary>
+    private static DialogHost? FindParentDialogHost(DependencyObject child)
+    {
+        var parent = VisualTreeHelper.GetParent(child);
+        
+        while (parent != null)
+        {
+            if (parent is DialogHost dialogHost)
+            {
+                return dialogHost;
+            }
+            parent = VisualTreeHelper.GetParent(parent);
+        }
+        
+        return null;
     }
 }
