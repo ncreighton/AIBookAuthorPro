1efb591af31f0d06954d281bc3cbfee4cce0aa6c
ncreighton <n.creighton14@gmail.com>
2025-11-27 23:02:01 -0500

    Fix: Settings dialog close button now works - subscribe to CloseRequested and call DialogHost.Close()

--- src/AIBookAuthorPro.UI/Views/SettingsView.xaml.cs
+++ src/AIBookAuthorPro.UI/Views/SettingsView.xaml.cs
@@ -3,8 +3,10 @@
 // Copyright (c) 2024 Nick Creighton. All rights reserved.
 // =============================================================================
 
+using System.Windows;
 using System.Windows.Controls;
 using AIBookAuthorPro.UI.ViewModels;
+using MaterialDesignThemes.Wpf;
 using UserControl = System.Windows.Controls.UserControl;
 
 namespace AIBookAuthorPro.UI.Views;
@@ -25,5 +27,30 @@ public partial class SettingsView : UserControl
         DataContext = _viewModel;
         
         InitializeComponent();
+
+        // Subscribe to the close request from ViewModel
+        _viewModel.CloseRequested += OnCloseRequested;
+
+        // Load settings when the view is loaded
+        Loaded += OnLoaded;
+        Unloaded += OnUnloaded;
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        // Load settings into the ViewModel
+        _viewModel.LoadSettings();
+    }
+
+    private void OnUnloaded(object sender, RoutedEventArgs e)
+    {
+        // Unsubscribe to prevent memory leaks
+        _viewModel.CloseRequested -= OnCloseRequested;
+    }
+
+    private void OnCloseRequested(object? sender, EventArgs e)
+    {
+        // Close the MaterialDesign DialogHost
+        DialogHost.Close("MainDialogHost", false);
     }
 }
